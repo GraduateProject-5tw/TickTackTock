2ecc92602e90be58847b984571b1c11c34a2340f
ming264 <watashi0604@gmail.com>
2021-04-13 04:41:03 +0800

    橫幅顯示成功。

--- app/src/main/java/com/GraduateProject/TimeManagementApp/NotificationService.java
+++ app/src/main/java/com/GraduateProject/TimeManagementApp/NotificationService.java
@@ -8,67 +8,73 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import androidx.core.app.NotificationCompat;
-
 import java.util.Timer;
 import java.util.TimerTask;
-public class NotificationService extends Service {
+
+public class NotificationService extends Service {    //server是一個在背景執行的服務，透過bindservice create、startservice start
+
     public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
     private final static String default_notification_channel_id = "default" ;
     Timer timer ;
     TimerTask timerTask ;
     String TAG = "Timers" ;
-    int Your_X_SECS = 5 ;
+    int Your_X_SECS = 4 ;
+
     @Override
-    public IBinder onBind (Intent arg0) {
+    public IBinder onBind (Intent arg0) {  //將app綁定server服務
         return null;
     }
+
     @Override
-    public int onStartCommand (Intent intent , int flags , int startId) {
-        Log. e ( TAG , "onStartCommand" ) ;
-        super .onStartCommand(intent , flags , startId) ;
-        startTimer() ;
-        return START_STICKY ;
-    }
-    @Override
-    public void onCreate () {
+    public void onCreate () {   //一旦離開app，建立server服務
         Log. e ( TAG , "onCreate" ) ;
     }
+
+    @Override
+    public int onStartCommand (Intent intent , int flags , int startId) {  //建立以後，啟動server服務
+        Log. e ( TAG , "onStartCommand" ) ;
+        startTimer() ;  //設定計時器
+        super .onStartCommand(intent , flags , startId) ;
+        return START_STICKY ;
+    }
+
     @Override
     public void onDestroy () {
         Log. e ( TAG , "onDestroy" ) ;
-        stopTimerTask() ;
         super .onDestroy() ;
     }
-    //we are going to use a handler to be able to run in our TimerTask
+
+    //用 handler 處理計時器任務
     final Handler handler = new Handler() ;
+
+    //建立計時器
     public void startTimer () {
         timer = new Timer() ;
-        initializeTimerTask() ;
-        timer .schedule( timerTask , 5000 , Your_X_SECS * 1000 ) ; //
+        initializeTimerTask ();
+        timer .schedule( timerTask , 10 , Your_X_SECS * 5000 ) ; //每5秒執行一次task
     }
-    public void stopTimerTask () {
-        if ( timer != null ) {
-            timer .cancel() ;
-            timer = null;
-        }
-    }
+
+    //時間內，任務要做的任務
     public void initializeTimerTask () {
+        long t0 = System.currentTimeMillis();
         timerTask = new TimerTask() {
             public void run () {
-                handler .post( new Runnable() {
-                    public void run () {
-                        createNotification() ;
-                    }
-                }) ;
+                if (System.currentTimeMillis() - t0 > 3 * 1000) {  //若時間大於3秒
+                    cancel();   //關閉任務
+                } else {
+                    createNotification ();  //跳出通知
+                }
             }
         } ;
     }
+
+    //跳出通知
     private void createNotification () {
         NotificationManager mNotificationManager = (NotificationManager) getSystemService( NOTIFICATION_SERVICE ) ;
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext() , default_notification_channel_id ) ;
-        mBuilder.setContentTitle( "警告" ) ;
-        mBuilder.setContentText( "停止計時 時間歸0" ) ;
-        mBuilder.setTicker( "Notification Listener Service Example" ) ;
+        mBuilder.setContentTitle( "時間通知" ) ;
+        mBuilder.setContentText( "停止計時" ) ;
+        mBuilder.setTicker( "停止計時" ) ;
         mBuilder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
         mBuilder.setAutoCancel( true ) ;
         if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
@@ -81,5 +87,4 @@ public class NotificationService extends Service {
         assert mNotificationManager != null;
         mNotificationManager.notify(( int ) System. currentTimeMillis () , mBuilder.build()) ;
     }
-
 }
--- app/src/main/java/com/GraduateProject/TimeManagementApp/StopWatchActivity.java
+++ app/src/main/java/com/GraduateProject/TimeManagementApp/StopWatchActivity.java
@@ -1,21 +1,21 @@
 package com.GraduateProject.TimeManagementApp;
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.lifecycle.LifecycleObserver;
 import java.util.concurrent.TimeUnit;
 
-public class StopWatchActivity extends AppCompatActivity {
+public class StopWatchActivity extends AppCompatActivity implements LifecycleObserver {
 
     private Chronometer chronometer; //計時器
     private Button startBtn, stopBtn;
     private long recordTime;  //累計的時間
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,36 +25,36 @@ public class StopWatchActivity extends AppCompatActivity {
         stopBtn = findViewById(R.id.stop_btn);
 
         //計時按鈕的功能實作
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime());  //計時器歸0
-                chronometer.start();
-                startBtn.setVisibility(View.GONE);
-                stopBtn.setVisibility(View.VISIBLE);
-            }
+        startBtn.setOnClickListener(v -> {
+            chronometer.setBase(SystemClock.elapsedRealtime());  //計時器歸0
+            chronometer.start();
+            startBtn.setVisibility(View.GONE);
+            stopBtn.setVisibility(View.VISIBLE);
+
         });
 
         //停止按鈕的功能實作
-        stopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chronometer.stop();
-                recordTime = SystemClock.elapsedRealtime() - chronometer.getBase();  //取得累計時間，單位是毫秒
-                String Time = getDurationBreakdown(recordTime);  //轉成小時分鐘秒
-                startBtn.setVisibility(View.VISIBLE);
-                stopBtn.setVisibility(View.GONE);
-                //跳出視窗
-                AlertDialog.Builder builder = new AlertDialog.Builder(StopWatchActivity.this);
-                AlertDialog dialog = builder.create();
-                dialog.setMessage("本次累積："+ Time);
-                dialog.setCanceledOnTouchOutside(true); //允許按對話框外部來關閉視窗
-                dialog.show();
-                recordTime = 0;
-                Time = "";
-                chronometer.setBase(SystemClock.elapsedRealtime()); //將計時器歸0
-            }
+        stopBtn.setOnClickListener(v -> {
+            chronometer.stop();
+            recordTime = SystemClock.elapsedRealtime() - chronometer.getBase();  //取得累計時間，單位是毫秒
+            String Time = getDurationBreakdown(recordTime);  //轉成小時分鐘秒
+            startBtn.setVisibility(View.VISIBLE);
+            stopBtn.setVisibility(View.GONE);
+            //跳出視窗
+            AlertDialog.Builder builder = new AlertDialog.Builder(StopWatchActivity.this);
+            AlertDialog dialog = builder.create();
+            dialog.setMessage("本次累積："+ Time);
+            dialog.setCanceledOnTouchOutside(true); //允許按對話框外部來關閉視窗
+            dialog.show();
+            recordTime = 0;
+            chronometer.setBase(SystemClock.elapsedRealtime()); //將計時器歸0
         });
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
     }
 
     public static String getDurationBreakdown(long millis) {
@@ -77,14 +77,5 @@ public class StopWatchActivity extends AppCompatActivity {
         sb.append(" 秒");
 
         return(sb.toString());
-    }
-
-    @Override
-    protected void onStop () {
-        super .onStop() ;
-        startService( new Intent( this, NotificationService. class )) ;
-    }
-    public void closeApp (View view) {
-        finish() ;
     }
 }
