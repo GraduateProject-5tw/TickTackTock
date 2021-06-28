f6074b5ddfa6adc92c12b8921b1582429720af0c
lavee903 <76218638+lavee903@users.noreply.github.com>
2021-06-28 13:30:22 +0800

    mine

---
+++ app/src/main/java/com/GraduateProject/TimeManagementApp/AppListAdapter.java
@@ -0,0 +1,60 @@
+package com.GraduateProject.TimeManagementApp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AppListAdapter extends RecyclerView.Adapter<AppListAdapter.ViewHolder> {
+    List<AppInfo> appList;
+    Context context;
+    AppListAdapter(List<AppInfo> appInfos){
+        this.appList=appInfos;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i){
+        if(context==null)
+            context=viewGroup.getContext();
+        View view= LayoutInflater.from(context).inflate(R.layout.app_card,viewGroup,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i){
+        viewHolder.appLogo.setImageDrawable(appList.get(i).appLogo);
+        viewHolder.appName.setText(appList.get(i).appName);
+        if(appList.get(i).appStatus)
+            viewHolder.appStatus.setImageResource(R.drawable.ic_lock);
+        else
+            viewHolder.appStatus.setImageResource(R.drawable.ic_lock_open);
+    }
+
+    @Override
+    public int getItemCount(){
+        return appList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView appLogo,appStatus;
+        TextView appName;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            appName=itemView.findViewById(R.id.app_name);
+            appStatus=itemView.findViewById(R.id.app_status);
+            appLogo=itemView.findViewById(R.id.app_logo);
+
+        }
+    }
+
+
+}
